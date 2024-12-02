f8ef81331e117520f979cb47f7156fe7cffc36d5
Brice KENGNI ZANGUIM <kenzabri2@yahoo.com>
2024-12-02 20:25:59 +0100

    Chargement Automatique du Google Sheet

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,11 +1,22 @@
+###################################################################################################################
+###################################################################################################################
 import streamlit as st
 import pandas as pd
 
 
+###################################################################################################################
+###################################################################################################################
 # Fonction pour charger et analyser les données
-def load_and_analyze_data(file):
+def load_and_analyze_data( ):
+    sheet_name = "Enquete_legumes_d_Afrique" # replace with your own sheet name
+    sheet_id = '1tjN9K9KeY5Eb6G3J-IFJ5HUmZQuGjZh9ZvT-hZUMpF8' # replace with your sheet's ID
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    
+    # Permettre à l'utilisateur de télécharger un fichier CSV
+    st.sidebar.title(f"URL : {url}")
+
     # Charger le fichier CSV
-    df = pd.read_csv(file)
+    df = pd.read_csv(url)
     
     # Suppression du TimeStamp
     df.drop(["Timestamp"], axis = 1, inplace = True)
@@ -20,7 +31,7 @@ def load_and_analyze_data(file):
     
     # Afficher un aperçu des données
     st.sidebar.subheader("4 -   Aperçu des données")
-    st.sidebar.write(df.head())
+    st.sidebar.dataframe(df.head())
 
     if column:
         st.subheader(f"I -   **Analyse de la colonne : {column}**")
@@ -37,7 +48,7 @@ def load_and_analyze_data(file):
 
         # Afficher le tableau avec les fréquences et les pourcentages
         st.subheader("II -   **Valeurs uniques, leur fréquence et pourcentage associé :**")
-        st.write(result)
+        st.dataframe(result)
 
         # Si la colonne est numérique, afficher des statistiques descriptives
         if pd.api.types.is_numeric_dtype(df[column]):
@@ -51,16 +62,18 @@ def load_and_analyze_data(file):
 
         # Visualisation de la distribution des valeurs
         st.subheader("III -  Visualisation des valeurs")
-        st.bar_chart(unique_values)
+        st.bar_chart(unique_values )
 
+
+###################################################################################################################
+###################################################################################################################
 # Interface utilisateur
 st.title("Analyse des données du sondage 'Enquête légumes d'Afrique'")
 
-# Permettre à l'utilisateur de télécharger un fichier CSV
-st.sidebar.title("1 -   Cliquer ici Pour charger le fichier à analyser : ")
-uploaded_file = st.sidebar.file_uploader("", type=["csv"])
 
-if uploaded_file:
-    # Analyser les données une fois le fichier téléchargé
-    load_and_analyze_data(uploaded_file)
+# Permettre à l'utilisateur de télécharger un fichier CSV
+st.sidebar.title("1 -   Le fichier Analalysé est directement chargé depuis GoogleSheet : ")
+    
+# Analyser les données une fois le fichier téléchargé
+load_and_analyze_data( )
 
